92652d4e63618fdfca9687f6978124da15be2e99
y.kapsha <y.kapsha@telda.ru>
2023-06-09 01:05:37 +0400

    - editor: toolbar

--- src/app/modules/editor/components/slide-editor/components/editor-toolbar/editor-toolbar.component.ts
+++ src/app/modules/editor/components/slide-editor/components/editor-toolbar/editor-toolbar.component.ts
@@ -3,7 +3,7 @@ import { EditorState, SlideType } from '../../../../state/editor.state';
 import { Store } from '@ngrx/store';
 import { editorActions } from '../../../../state/editor.actions';
 import { FormControl, Validators } from '@angular/forms';
-import { Observable, take } from 'rxjs';
+import { Observable, Subject, take, takeUntil } from 'rxjs';
 import {
   selectCurrentSlide,
   selectCurrentSlideStats,
@@ -18,8 +18,10 @@ import { map } from 'rxjs/operators';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class EditorToolbarComponent implements OnInit {
-  slideTypes = SlideType;
-  pointControl = new FormControl(10, [Validators.min(0)]);
+  public slideTypes = SlideType;
+  public pointControl = new FormControl(10, [Validators.min(0)]);
+
+  private destroy$ = new Subject<void>();
 
   public selectEditor: Observable<EditorState> =
     this.store.select(selectEditor);
@@ -28,21 +30,113 @@ export class EditorToolbarComponent implements OnInit {
 
   public selectCurrentSlideStats = this.store.select(selectCurrentSlideStats);
 
+  public isToxic = this.currentSlide.pipe(
+    // @ts-ignore
+    map((slideWrapper) => slideWrapper.slide?.toxic),
+  );
+
   constructor(private store: Store) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.pointControl.valueChanges
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((value) => {
+        this.currentSlide.pipe(take(1)).subscribe((slideWrapper) => {
+          if (slideWrapper.slide) {
+            this.store.dispatch(
+              editorActions.updateslide({
+                slideIndex: slideWrapper.slideIndex,
+                slide: {
+                  ...slideWrapper.slide,
+                  // @ts-ignore
+                  points: value,
+                },
+              }),
+            );
+          }
+        });
+      });
+
+    this.currentSlide
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((slideWrapper) => {
+        // @ts-ignore
+        if (slideWrapper.slide?.points) {
+          // @ts-ignore
+          this.pointControl.setValue(slideWrapper.slide.points, {
+            emitEvent: false,
+          });
+        }
+      });
+  }
 
   addSlide(slideType: SlideType) {
     this.store.dispatch(editorActions.addslide({ slideType }));
   }
 
-  moveSlideUp() {}
+  moveSlideUp() {
+    this.selectEditor.pipe(take(1)).subscribe((editor) => {
+      if (editor.currentSlideIndex > 0) {
+        this.store.dispatch(
+          editorActions.moveslide({
+            slideIndex: editor.currentSlideIndex,
+            newIndex: editor.currentSlideIndex - 1,
+          }),
+        );
+      }
+    });
+  }
 
-  moveSlideDown() {}
+  moveSlideDown() {
+    this.selectEditor.pipe(take(1)).subscribe((editor) => {
+      if (editor.currentSlideIndex < editor.slides.length - 1) {
+        this.store.dispatch(
+          editorActions.moveslide({
+            slideIndex: editor.currentSlideIndex,
+            newIndex: editor.currentSlideIndex + 1,
+          }),
+        );
+      }
+    });
+  }
 
-  removeSlide() {}
+  removeSlide() {
+    this.selectEditor.pipe(take(1)).subscribe((editor) => {
+      if (editor.currentSlideIndex !== null) {
+        this.store.dispatch(
+          editorActions.deleteslide({ slideIndex: editor.currentSlideIndex }),
+        );
+      }
+    });
+  }
 
-  toxic() {}
+  isQuestionSlide(): Observable<boolean> {
+    return this.selectCurrentSlideStats.pipe(
+      map((stats) => {
+        return (
+          stats?.slide.type === SlideType.questionWithImage ||
+          stats?.slide.type === SlideType.questionWithAudio
+        );
+      }),
+    );
+  }
+
+  setToxic() {
+    this.currentSlide.pipe(take(1)).subscribe((slideWrapper) => {
+      if (slideWrapper) {
+        this.store.dispatch(
+          editorActions.updateslide({
+            slide: {
+              ...slideWrapper.slide,
+              // @ts-ignore
+              toxic: !slideWrapper.slide.toxic,
+            },
+            slideIndex: slideWrapper.slideIndex,
+          }),
+        );
+      }
+    });
+  }
 
   addPatch() {
     let newPatch = {
